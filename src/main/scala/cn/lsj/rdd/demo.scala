e1f39d2c71127c5bb2eae61fbecdd73dd3a8cf0e
luoshijun <luoshijun58@163.com>
2017-09-26 10:50:09 +0800

    update

--- src/main/scala/cn/lsj/rdd/demo.scala
+++ src/main/scala/cn/lsj/rdd/demo.scala
@@ -28,12 +28,14 @@ object init {
      **/
     def initSpark(masterUrl: String, appName: String): (SparkContext, SQLContext) = {
         //    val conf = new SparkConf().setMaster("yarn-client").setAppName("Test")
-        val conf = new SparkConf().setMaster(masterUrl).setAppName(appName)
+        val conf = new SparkConf()
+//          .setMaster(masterUrl).setAppName(appName)
         //==================优化参数，根据业务及数据量大小调整============================
         conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer") //序列化类
         // 注册自定义类型，默认只支持常用类型，如有比较复杂的类型及自定义类型需要注册
         //    conf.set("spark.kryo.registrator", "com.ultrapower.noas.javaspark.tools.Registorator");
         //决定了能够传输消息的最大值（默认为10M），单位M，当应用里有需要传输大数据量的时候可以调整此参数
+        //不能越过2G
         conf.set("spark.akka.frameSize", "100")
         //Spark内存缓存的堆大小占用总内存比例（默认0.6），不能大于JVM Old区内存大小
         conf.set("spark.storage.memoryFraction", "0.4")
@@ -45,7 +47,10 @@ object init {
         conf.set("spark.shuffle.compress", "true") // 是否压缩map操作的输出文件。
         conf.set("spark.shuffle.spill.compress", "true") // 在shuffle时，是否将spilling的数据压缩。压缩算法通过spark.io.compression.codec指定。
         conf.set("spark.rdd.compress", "true") // 是否压缩序列化的RDD分区。在花费一些额外的CPU时间的同时节省大量的空间
-        //    conf.set("spark.default.parallelism", "100") //shuffle过程默认使用的task数量（可以简单的理解为reduce个数，默认为8）,如果是sql的话，可修改spark.sql.shuffle.partitions
+        //shuffle过程默认使用的task数量（可以简单的理解为reduce个数，默认为8）,
+        //如果是sql的话，可修改spark.sql.shuffle.partitions
+        //在 partition 大于 2000 时，会有另一种更高效 [压缩] 的数据结构来存储信息，如果接近小于这个值可以考虑设置为此值
+        //    conf.set("spark.default.parallelism", "100")
         //==================优化参数============================
         val sc = new SparkContext(conf)
         val sqlContext = new SQLContext(sc)
@@ -69,13 +74,14 @@ class CoursesSum() extends Serializable {
 +--------------+---------------+------+-----+---------+-------+
      **/
     def run(): Unit = {
-        val inFiles = List("E:\\git\\my-spark\\testData\\test.courses")
-        val outFiles = List("E:\\git\\my-spark\\testData\\test.courses.sum1",
-            "E:\\git\\my-spark\\testData\\test.courses.sum2",
-            "E:\\git\\my-spark\\testData\\test.courses.join1",
-            "E:\\git\\my-spark\\testData\\test.courses.leftjoin",
-            "E:\\git\\my-spark\\testData\\test.courses.rightjoin",
-            "E:\\git\\my-spark\\testData\\test.courses.mapjoin"
+        //打包执行（spark-submit）时，默认使用HDFS文件系统，如需使用本地文件需使用file前缀，output文件不能是file
+        val inFiles = List("file:///E:\\git\\my-spark\\testData\\test.courses")
+        val outFiles = List("/tmp/spark/test.courses.sum1",
+            "/tmp/spark/test.courses.sum2",
+            "/tmp/spark/test.courses.join1",
+            "/tmp/spark/test.courses.leftjoin",
+            "/tmp/spark/test.courses.rightjoin",
+            "/tmp/spark/test.courses.mapjoin"
         )
         val (sc, sqlContext) = init.initSpark("local", "coureseSum")
 
@@ -112,7 +118,8 @@ class CoursesSum() extends Serializable {
           .map((cols) => ((cols(1), cols(3), cols(6).toLong), 1)) //处理为K，V（(level,url,student),1），方便去重
           .reduceByKey((a, b) => a) //数据去重
           .map((row) => (row._1._1, (row._1._3, row._2))) //重新构建K，V()
-          .reduceByKey((a, b) => (a._1 + b._1, a._2 + b._2))
+//          .reduceByKey((a, b) => (a._1 + b._1, a._2 + b._2))
+          .aggregateByKey((a, b) => (a._1 + b._1, a._2 + b._2))
           .map((row) => List(row._1, row._2._1, row._2._2).mkString(",")).coalesce(1) // 处理文件输入格式
 //        levelStudents.saveAsTextFile(outFiles(1))
 
@@ -177,8 +184,8 @@ class CoursesSum() extends Serializable {
 
 
 
-        Thread.sleep(10000000)
-
+//        Thread.sleep(10000000)
+        sc.stop()
     }
 
 
